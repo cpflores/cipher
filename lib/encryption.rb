ceb32e0d2c9d430d9834f6f26271cd2f5f4ef766
Chadwick Flores <chadwickflores@gmail.com>
2014-04-30 20:01:52 -0500

    four tests passed

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,2 +1,24 @@
 class Encryption
+	def encrypt(input_string)
+		uppercase = true
+		if(input_string.ord > 'Z'.ord)
+			uppercase = false
+		end
+
+		if (uppercase)
+			threshold = 'M'.ord
+			if(input_string.ord > threshold)
+				(input_string.ord - 13).chr
+			else
+				(input_string.ord + 13).chr
+			end
+		else
+			threshold = 'm'.ord
+			if(input_string.ord > threshold)
+				(input_string.ord - 13).chr
+			else
+				(input_string.ord + 13).chr
+			end
+		end
+	end
 end
--- spec/encryption_spec.rb
+++ spec/encryption_spec.rb
@@ -6,5 +6,19 @@ require "spec_helper"
 			encryption = Encryption.new
 			encryption.encrypt("Hello World").should be_a String
 		end
+		it "encrypts a string containing the letter a into a string containing the letter n" do
+			encryption = Encryption.new
+			encryption.encrypt("a").should == "n"
+		end
+
+		it "encrypts a string containing the letter an uppercase A into N" do
+			encryption = Encryption.new
+			encryption.encrypt("A").should == "N"
+		end
+
+		it "encrypts a z into m" do
+			encryption = Encryption.new
+			encryption.encrypt("z").should == "m"
+		end 
 
 end
