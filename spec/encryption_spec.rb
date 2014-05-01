84d63a7b1c490f47dbe0ca4709c3af06e21f5eaf
Chadwick Flores <chadwickflores@gmail.com>
2014-04-30 19:23:45 -0500

    cipher project

--- spec/encryption_spec.rb
+++ spec/encryption_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 	describe Encryption do
 
 		it "encrypts a String and returns a String" do
-			encyrption = Encryption.new
+			encryption = Encryption.new
 			encryption.encrypt("Hello World").should be_a String
 		end
 
